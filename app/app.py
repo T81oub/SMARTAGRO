d6e82a0b64a52d9c1f00c0b4fa7555a09348d1c3
T81oub <93501885+T81oub@users.noreply.github.com>
2023-07-02 01:15:47 +0100

    Updated 2/7  files

--- app/app.py
+++ app/app.py
@@ -1,16 +1,91 @@
 
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request, Markup,redirect
 import numpy as np
 import pandas as pd
 import pickle
 import os
+import io
+import torch
+from torchvision import transforms
+from PIL import Image
+from models.model import ResNet9
+
+
+disease_classes = ['Apple___Apple_scab',
+                   'Apple___Black_rot',
+                   'Apple___Cedar_apple_rust',
+                   'Apple___healthy',
+                   'Blueberry___healthy',
+                   'Cherry_(including_sour)___Powdery_mildew',
+                   'Cherry_(including_sour)___healthy',
+                   'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+                   'Corn_(maize)___Common_rust_',
+                   'Corn_(maize)___Northern_Leaf_Blight',
+                   'Corn_(maize)___healthy',
+                   'Grape___Black_rot',
+                   'Grape___Esca_(Black_Measles)',
+                   'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+                   'Grape___healthy',
+                   'Orange___Haunglongbing_(Citrus_greening)',
+                   'Peach___Bacterial_spot',
+                   'Peach___healthy',
+                   'Pepper,_bell___Bacterial_spot',
+                   'Pepper,_bell___healthy',
+                   'Potato___Early_blight',
+                   'Potato___Late_blight',
+                   'Potato___healthy',
+                   'Raspberry___healthy',
+                   'Soybean___healthy',
+                   'Squash___Powdery_mildew',
+                   'Strawberry___Leaf_scorch',
+                   'Strawberry___healthy',
+                   'Tomato___Bacterial_spot',
+                   'Tomato___Early_blight',
+                   'Tomato___Late_blight',
+                   'Tomato___Leaf_Mold',
+                   'Tomato___Septoria_leaf_spot',
+                   'Tomato___Spider_mites Two-spotted_spider_mite',
+                   'Tomato___Target_Spot',
+                   'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+                   'Tomato___Tomato_mosaic_virus',
+                   'Tomato___healthy']
+
 app=Flask(__name__)
 fertilizer_predict_model_path='models/fertilizer.pkl'
 crop_predict_path="models/cropv2.pkl"
+#####################################################################
+disease_model_path = 'models/plant_disease_model.pth'
 
+
+disease_model = ResNet9(3, len(disease_classes))
+disease_model.load_state_dict(torch.load(
+    disease_model_path, map_location=torch.device('cpu')))
+disease_model.eval()
+
+def predict_image(img, model=disease_model):
+    """
+    Transforms image to tensor and predicts disease label
+    :params: image
+    :return: prediction (string)
+    """
+    transform = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+    ])
+    image = Image.open(io.BytesIO(img))
+    img_t = transform(image)
+    img_u = torch.unsqueeze(img_t, 0)
+
+    # Get predictions from model
+    yb = model(img_u)
+    # Pick index with highest probability
+    _, preds = torch.max(yb, dim=1)
+    prediction = disease_classes[preds[0].item()]
+    # Retrieve the class label
+    return prediction
+#####################################################################
 
 diseases_model_path='models/diseasemodel.pth'
-diseases_model = ResNet9(3, len(disease_classes))
 
 folder = os.path.join('static','cropimages')
 app.config['UPLOAD_FOLDER'] = folder
@@ -31,7 +106,9 @@ def fertilizer_recommendation():
 @app.route('/crop')
 def crop_recommendation():
     return render_template('crop.html')
-
+@app.route('/disease')
+def disease_recommendation():
+    return render_template('disease.html')
 @app.route("/fertilizer_predict",methods=['POST'])
 def fertilizer_predict():
     if request.method == 'POST':
@@ -84,5 +161,27 @@ def crop_predict():
         ans=modelcrop.predict(data)
         imagepath = os.path.join(app.config['UPLOAD_FOLDER'], ans[0]+'.jpg')
         return render_template('crop-result.html',prediction=ans[0],image=imagepath)
+    
+@app.route('/disease_predict', methods=['POST'])
+def disease_predict():
+    
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return render_template('disease.html')
+        
+        img = file.read()
+
+         
+
+        prediction = predict_image(img)
+        
+
+        return render_template('crop-result.html', prediction=prediction)
+        
+    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
